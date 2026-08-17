94b41f0597f132b2b696a8fc706aec05e2701b19
sulim6426-arch <sulim6426@gmail.com>
2026-08-16 21:29:45 -0700

    Create VoiceService.java

---
+++ app/src/main/java/com/myapp/commandexecutor/VoiceService.java
@@ -0,0 +1,14 @@
+package com.myapp.commandexecutor;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import androidx.annotation.Nullable;
+
+public class VoiceService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
