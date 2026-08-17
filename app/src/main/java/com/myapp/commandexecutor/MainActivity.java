14b9f74f3a692abd5929edf8c0ee24a3bbe56e4e
sulim6426-arch <sulim6426@gmail.com>
2026-08-16 21:28:56 -0700

    Create MainActivity.java

---
+++ app/src/main/java/com/myapp/commandexecutor/MainActivity.java
@@ -0,0 +1,12 @@
+package com.myapp.commandexecutor;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // يمكنك ترك القالب الأساسي هنا أو وضع كود واجهتك
+    }
+}
